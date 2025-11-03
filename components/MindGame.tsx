82a2976dd08a9b52b87968c98f744483db293f16
sameer4712 <sameerptmc@gmail.com>
2025-11-03 13:05:18 +0530

    font changes

--- components/MindGame.tsx
+++ components/MindGame.tsx
@@ -51,24 +51,23 @@ export default function MindGame() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden bg-black">
+    <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden ">
       <h1 className="text-red-400 text-4xl font-bold font-mono mt-4 mb-4">
         Mind <span className="text-yellow-300">Game</span>
       </h1>
 
       {matchWinner && (
-        <h2 className="text-green-500 text-2xl mb-4 ml-9 font-bold font-mono">You Won!! 🎉</h2>
+        <h2 className="text-green-400 text-2xl mb-4 ml-9 font-bold font-mono">You Won!! 🎉</h2>
       )}
 
       <div className="grid grid-cols-3 md:grid-cols-4 gap-5 mb-4">
         {cards.map((card, index) => (
           <div
             key={index}
-            className={`w-24 h-24 md:w-32 md:h-32 relative border border-gray-600 rounded-2xl cursor-pointer text-white flex justify-center items-center font-bold text-4xl transition-transform duration-300 ease-in-out ${
-              flipped.includes(index) || solved.includes(index)
+            className={`w-24 h-24 md:w-32 md:h-32 relative border bg-slate-700 border-gray-600 rounded-3xl font-mono cursor-pointer text-amber-50 flex justify-center items-center font-bold text-5xl transition-transform duration-300 ease-in-out ${flipped.includes(index) || solved.includes(index)
                 ? "rotate-180"
                 : ""
-            }`}
+              }`}
             onClick={() => checkCard(index)}
           >
             {flipped.includes(index) || solved.includes(index) ? (
@@ -76,7 +75,7 @@ export default function MindGame() {
                 src={`/memory-card/${card}.jpg`}
                 fill
                 alt="cards"
-                className="rounded-2xl rotate-180 object-cover"
+                className="rounded-3xl rotate-180 object-cover"
               />
             ) : (
               "?"
@@ -87,7 +86,7 @@ export default function MindGame() {
 
       <button
         onClick={restartMatch}
-        className="text-red-700 text-xl bg-slate-200  border border-gray-400 font-bold cursor-pointer outline-none px-6 py-2 rounded-lg hover:bg-red-700 hover:text-white"
+        className="text-red-700 text-xl bg-slate-200  border border-gray-400 font-bold font-mono cursor-pointer outline-none px-6 py-2 my-2 rounded-lg hover:bg-red-700 hover:text-white"
       >
         Reset Game
       </button>
